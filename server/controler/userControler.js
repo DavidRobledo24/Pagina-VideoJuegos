5c082130212e92807274980b019fc8041ee0cbf5
DavidRobledo24 <darobledo98@soy.sena.edu.co>
2024-05-27 16:22:37 -0500

    pre-despliegue

--- server/controler/userControler.js
+++ server/controler/userControler.js
@@ -1,20 +1,99 @@
-const fs = require('fs').promises;
-const { register } = require('module');
-const path = require('path');
+// const fs = require('fs').promises;
+// const { register } = require('module');
+// const path = require('path');
 
-const userFilePath = path.join(__dirname,'../../src/componentes/usuariosReistrados.json')
+// const userFilePath = path.join(__dirname,'../../src/componentes/usuariosReistrados.json')
 
-const controller ={
-    register: async function(req, res){
-        console.log(req);
-        try{
+// const controller ={
+//     register: async function(req, res){
+//         console.log(req);
+//         try{
 
-            const userData = await fs.readFile(userFilePath, "utf-8");
-            const users = JSON.parse(userData);
+//             const userData = await fs.readFile(userFilePath, "utf-8");
+//             const users = JSON.parse(userData);
             
-            const ultimo = users.length;
-            const usuarioNuevo = {
-                id: ultimo + 1,
+//             const ultimo = users.length;
+//             const usuarioNuevo = {
+//                 id: ultimo + 1,
+//                 nombres: req.body.nombres,
+//                 apellidos: req.body.apellidos,
+//                 email: req.body.email,
+//                 direccion: req.body.direccion,
+//                 telefono: req.body.telefono,
+//                 fechaNacimiento: req.body.fechaNacimiento,
+//                 password: req.body.password,
+//                 estado: "Activo",
+//                 rol: "Usuario",
+//                 fechaCreacion: new Date()
+//             };
+
+//             for(x of users){
+//             if(x.email ===req.body.email){
+//                 res.status(400).send("El email ya existe");
+//                 return
+//             }
+//         }
+//         users.push(usuarioNuevo);
+        
+//         await fs.writeFile(userFilePath, JSON.stringify(users,null, 4));
+        
+//         res.status(200).send("Exito");
+//         } catch (error){
+//             console.error("Error al procesar el registro:", error);
+//             res.status(500).send("Error interno del servidor");
+//         }
+//     },
+
+
+//     login: async function (req, res){
+//         try{
+//             const usersData = await fs.readFile(userFilePath, "utf-8");
+//             const users = JSON.parse(usersData);
+
+//             for(x of users){
+//                 if(
+//                     x.email === req.body.email &&
+//                     x.password === req.body.password &&
+//                     x.rol === req.body.rol 
+//                 ){
+//                    return res.json({
+//                     nombres: x.nombres,
+//                     apellidos: x.apellidos,
+//                     email: x.email,
+//                    });
+//                 }
+//             }
+//             res.json({title:"error"});
+//         } catch (Error){
+//             console.Error("Error al procesar el registro:", Error);
+//             res.status(500).send("Error interno del servidor");
+//         }
+//     },
+
+// };
+
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cors = require('cors');
+app.use(cors());
+
+const controller = {
+    register: function (req, res) {
+      let config = {
+        method: "GET",
+        maxBodyLength: Infinity,
+        url: 'https://api.jsonbin.io/v3/b/6654d62facd3cb34a84e8a70',
+        headers: {
+          'Content-Type': 'application/json',
+          "X-Master-Key": "$2a$10$rhax496YXf/0IQXYzz4CTujv/vVetDrw4pJbOrwjq4/aTUhDePQSe"
+        }
+      };
+      axios(config)
+        .then(result => {
+          let id = result.data.record.length + 1
+          const usuarioNuevo = {
+                id: id,
                 nombres: req.body.nombres,
                 apellidos: req.body.apellidos,
                 email: req.body.email,
@@ -26,71 +105,39 @@ const controller ={
                 rol: "Usuario",
                 fechaCreacion: new Date()
             };
-
-            for(x of users){
-            if(x.email ===req.body.email){
-                res.status(400).send("El email ya existe");
-                return
+            if (result.data.record.length === 0) {
+              result.data.record.push(usuarioNuevo)
             }
-        }
-        users.push(usuarioNuevo);
-        
-        await fs.writeFile(userFilePath, JSON.stringify(users,null, 4));
-        
-        res.status(200).send("Exito");
-        } catch (error){
-            console.error("Error al procesar el registro:", error);
-            res.status(500).send("Error interno del servidor");
-        }
-    },
-
-
-    login: async function (req, res){
-        try{
-            const usersData = await fs.readFile(userFilePath, "utf-8");
-            const users = JSON.parse(usersData);
-
-            for(x of users){
-                if(
-                    x.email === req.body.email &&
-                    x.password === req.body.password &&
-                    x.rol === req.body.rol 
-                ){
-                   return res.json({
-                    nombres: x.nombres,
-                    apellidos: x.apellidos,
-                    email: x.email,
-                   });
+            else {
+              for (x of result.data.record) {
+                if (x.email === req.body.email) {
+                  res.status(400).send("Usuario ya existe en la Base de Datos")
+                  return
                 }
+              }
+              result.data.record.push(usuarioNuevo)
             }
-            res.json({title:"error"});
-        } catch (Error){
-            console.Error("Error al procesar el registro:", Error);
-            res.status(500).send("Error interno del servidor");
-        }
-    },
-
-    // login: async function (req, res){
-    //     try{
-    //         const userData = await fs.readFile(userFilePath, "utf-8");
-    //         const users = JSON.parse(userData);
-
-    //         for(x of users){
-    //             if(
-    //                 x.email === req.body.email &&
-    //                 x.password === req.body.password &&
-    //                 x.rol === req.body.rol 
-    //             ){
-    //                 res.status(200).send("OK");
-    //                 return;
-    //             }
-    //         }
-    //         res.status(400).send("Error");
-    //     } catch (Error){
-    //         console.error("Error al procesar el registro:", error);
-    //         res.status(500).send("Error interno del servidor");
-    //     }
-    // },
-};
+    
+            fetch("https://api.jsonbin.io/v3/b/6654d62facd3cb34a84e8a70", {
+              method: "PUT",
+              headers: {
+                "Content-Type": "Application/json",
+                "X-Master-Key": "$2a$10$rhax496YXf/0IQXYzz4CTujv/vVetDrw4pJbOrwjq4/aTUhDePQSe"
+              },
+              body: JSON.stringify(result.data.record),
+            })
+              .then(response => {
+                if (response.status === 200) {
+                  res.status(200).send('ok')
+                  return
+                }
+                else {
+                  res.status(400).send("No Ok")
+                  return
+                }
+            })
+        })
+    }
+}
 
 module.exports = controller;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const axios = require("axios");
 
 app.use(bodyParser.urlencoded({expended: true}))
 app.use(express.json())
@@ -9,12 +10,23 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get("/", (req, res) =>{
-    res.send("saludando desde el backend")
+    let config ={
+        method: "GET",
+        url : "https://api.jsonbin.io/v3/b/6654d62facd3cb34a84e8a70",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Master-Key": "$2a$10$rhax496YXf/0IQXYzz4CTujv/vVetDrw4pJbOrwjq4/aTUhDePQSe"
+        }
+    }
+    axios(config)
+       .then(result =>{
+        res.send(result.data.record)
+    })
 })
 
 const user = require("./controler/userControler")
 app.use("/registro-usuario", user.register)
-app.use("/login",user.login)
+// app.use("/login",user.login)
 
 const PORT = 3001
 app.listen(PORT,()=>{
